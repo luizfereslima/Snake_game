cba796fbc619cd1e430c20989e7d86b6208292ef
Luiz Felipe Dias Cardoso Feres Lima <luiz0709.lfdcfl@gmail.com>
2023-09-08 08:55:32 -0300

    acerto

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
-let snake = [];
-snake[0] = {
-    X: 8 * box,
-    Y: 8 * box
-}
 
 function criarBG(){
-    context.fillStyle = "ligthgreen";
+    context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function criarCobrinha(){
-    for(i=0; i < snake.length; i++){
-        context.fillStyle = "green";
-        context.fillRect (snake[i].x, snake[i].y, box, box);
-    }
-}
-
 criarBG();
-criarCobrinha();
